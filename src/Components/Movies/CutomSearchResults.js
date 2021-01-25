c7b21f338ff719411e6088eecb71097a76eba4e3
gustavoleal-web <lealgustavo.web@gmail.com>
2021-01-25 11:47:17 -0600

    initial commit

---
+++ src/Components/Movies/CutomSearchResults.js
@@ -0,0 +1,27 @@
+import React, { useEffect, Fragment } from 'react';
+import { useParams } from 'react-router';
+import axios from 'axios';
+
+const CustomSearchResults = () => {
+    const params = useParams();
+    console.log( params );
+
+    useEffect( () => {
+        const fetchSearchOptions = async () => {
+            try {
+                const response = await axios.get( `/${ params.type }/releaseYear/language/?year=${ params.releaseYear }&originalLang=${ params.language }` );
+                let results = response.data.results;
+                console.log( results );
+            }
+
+            catch ( error ) {
+                console.log( error )
+            }
+        }
+        fetchSearchOptions();
+    }, [ params.type, params.releaseYear, params.language ] )
+
+    return <Fragment></Fragment>
+}
+
+export default CustomSearchResults;
