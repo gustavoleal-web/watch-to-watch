760dd8c414530a4a883d813facb458c1a5b7a332
gustavoleal-web <lealgustavo.web@gmail.com>
2021-08-14 17:09:35 -0500

    addded inline styles and display props

--- src/Trending/Movies/Movie/About/About.js
+++ src/Trending/Movies/Movie/About/About.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
-const About = ( { title, description, runtime, genres } ) => {
+const About = ( { title, description, runtime, genres, status } ) => {
     return (
         <div>
+
             <p>Runtime: { runtime } min.</p>
             <p>{ description }</p>
+
+            <div style={ { display: 'grid', gridTemplateColumns: '1fr 1fr' } }>
+                <h4 style={ { margin: '0' } }>Status</h4>
+                <h4 style={ { margin: '0' } }>Genres</h4>
+                <p>{ status }</p>
+                <ul style={ { listStyle: 'none', display: 'flex', flexWrap: 'wrap', paddingLeft: '0' } }>
+
+                    {
+                        genres.map( genre => <li key={ genre.id } style={{marginRight: '10px'}}>{ genre.name }.</li> )
+                    }
+                </ul>
+            </div>
+
+
         </div>
     )
 }
