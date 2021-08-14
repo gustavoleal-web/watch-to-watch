67d1383b10ce4f2d6f4cc458dcb5afb95e4323a9
gustavoleal-web <lealgustavo.web@gmail.com>
2021-08-14 18:08:12 -0500

    renderd new props with new styles

--- src/Trending/Movies/Movie/About/About.js
+++ src/Trending/Movies/Movie/About/About.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const About = ( { title, description, runtime, genres, status } ) => {
+const About = ( { title, description, runtime, genres, status, language, productionCountries } ) => {
     return (
         <div>
+            <div style={ { display: 'grid', gridTemplateColumns: '1fr 1fr 3fr 2fr' } }>
+                <p>Type</p>
+                <p>Lang</p>
+                <p>Produced In</p>
+                <p>Runtime</p>
 
-            <p>Runtime: { runtime } min.</p>
+                <p style={ { margin: '0' } }>Movie</p>
+                <p style={ { margin: '0' } }> { language.toUpperCase() }</p>
+                {
+                    productionCountries.map( company => <p key={ company.name } style={ { margin: '0' } }>{ company.iso_3166_1 }</p> )
+                }
+                <p style={ { margin: '0' } }> { runtime } min.</p>
+
+            </div>
+
+
             <p>{ description }</p>
 
             <div style={ { display: 'grid', gridTemplateColumns: '1fr 1fr' } }>
@@ -14,7 +28,7 @@ const About = ( { title, description, runtime, genres, status } ) => {
                 <ul style={ { listStyle: 'none', display: 'flex', flexWrap: 'wrap', paddingLeft: '0' } }>
 
                     {
-                        genres.map( genre => <li key={ genre.id } style={{marginRight: '10px'}}>{ genre.name }.</li> )
+                        genres.map( genre => <li key={ genre.id } style={ { marginRight: '10px' } }>{ genre.name }.</li> )
                     }
                 </ul>
             </div>
