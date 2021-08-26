8b9ead41bd04f08467fb95d0dc2532bf3f6a8b78
gustavoleal-web <lealgustavo.web@gmail.com>
2021-08-25 20:21:01 -0500

    moved styles to own file

--- src/Trending/Movies/Movie/Movie.js
+++ src/Trending/Movies/Movie/Movie.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './css/movie.module.css';
 
 const Movie = ( { title, releaseDate, posterPath, movieId } ) => {
-   
+
     return (
 
-        <div style={ { border: '1px solid black', marginBottom: '20px', marginLeft: '10px', marginRight: '10px' } }>
-
-            <div style={ { display: 'flex' } }>
-                <img src={ `https://image.tmdb.org/t/p/w300/${ posterPath }` } alt='poster' style={ { height: '200px' } } />
-                <div style={ { margin: 'auto' } }>
+        <div className={ styles.movieContainer }>
+            <div className={ styles.movieDisplay }>
+                <img src={ `https://image.tmdb.org/t/p/w300/${ posterPath }` } alt='poster' style={ { height: '200px ' } } />
+                <div className={ styles.movieMain }>
                     <h4>{ title }</h4>
                     <p>Release Date: { releaseDate }</p>
                 </div>
