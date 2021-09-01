c6cc946e2cf68edb0d23fdfab4a18995a17eb91f
gustavoleal-web <lealgustavo.web@gmail.com>
2021-09-01 17:53:08 -0500

    addded hook to fetch from api

--- src/Trending/TV/Show/About/About.js
+++ src/Trending/TV/Show/About/About.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const About = ( { genres, overview } ) => {
+    const [ state, setState ] = useState( {} )
+    const params = useParams();
+   
+    useEffect( () => {
+        const fetchShows = async () => {
+
+            try {
+                let response = await axios.get( `/trending/show/?showId=${ params.showId }` );
+                setState( response.data.results );
+
+                //props to pass About component
+                //genres
+                //first_air_date
+                //last_air_date
+                //last_episode_to_air
+                //next_episode_to_air (if any)
+                //number_of_episodes
+                //number_of_seasons
+                //seasons
+                //overview
+            }
+            catch ( e ) {
+                console.log( e )
+            }
+
+
+        }
+        fetchShows();
+    }, [ params.showId ] );
+
     return (
         <div>
-            <p>{ overview }</p>
         </div>
     )
 }
