aee65c82330bf550d1d223660c0a4ad5d4eb7ea6
gustavoleal-web <lealgustavo.web@gmail.com>
2021-10-07 20:22:48 -0500

    regex to move date order and img leads to link

--- src/Components/Movies/Movie.js
+++ src/Components/Movies/Movie.js
@@ -3,21 +3,27 @@ import { Link } from 'react-router-dom';
 import styles from './css/movie.module.css';
 
 const Movie = ( { title, releaseDate, posterPath, movieId, option } ) => {
-
     let linkPath = `/movies/${ option }/${ movieId }`;
+
+    //converts yyyy-mm-dd to mm/dd/yyyy
+    let regex = /(\d{4})-(\d{1,2})-(\d{1,2})/
+    let str = releaseDate;
+    let mmddyyyy = str.replace( regex, '$2/$3/$1' );
 
     return (
 
         <div className={ styles.movieContainer }>
             <div className={ styles.movieDisplay }>
-                <img src={ `https://image.tmdb.org/t/p/w300/${ posterPath }` } alt='poster' style={ { height: '200px ' } } />
+                <Link to={ linkPath }>
+                    <img src={ `https://image.tmdb.org/t/p/w300/${ posterPath }` } alt='poster' style={ { height: '200px ' } } />
+                </Link>
                 <div className={ styles.movieMain }>
                     <h4>{ title }</h4>
-                    <p>Release Date: { releaseDate }</p>
+                    <p>{ mmddyyyy }</p>
                 </div>
             </div>
 
-            <Link to={ linkPath }  >Load more movie info</Link>
+
 
         </div>
     )
