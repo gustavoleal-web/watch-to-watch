574c193d426c8d6c13878d5e67eebe9b4f04190d
gustavoleal-web <lealgustavo.web@gmail.com>
2021-10-13 16:24:53 -0500

    added inline styles

--- src/Components/Movies/Provider.js
+++ src/Components/Movies/Provider.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Fragment } from 'react';
 
 const Provider = ( { service, serviceName } ) => {
+    const style = {
+        display: 'flex',
+        backgroundColor: '#333',
+        overflow: 'auto',
+        whiteSpace: 'nowrap'
+    }
 
     if ( service.length === 0 ) {
         return null;
@@ -9,10 +15,10 @@ const Provider = ( { service, serviceName } ) => {
 
     return <Fragment>
         <h5>{ serviceName }</h5>
-        <div style={ { display: 'flex', flexWrap: 'wrap' } }>
+        <div style={ style }>
             {
                 service.map( s =>
-                    <div key={ s.provider_id } style={ { width: '100px' } }>
+                    <div key={ s.provider_id } style={ { width: '100px', padding: '10px' } }>
                         <img src={ `https://image.tmdb.org/t/p/original${ s.logo_path }` } alt="" style={ { height: '50px', width: '50px', borderRadius: '10px' } } />
                     </div>
                 )
