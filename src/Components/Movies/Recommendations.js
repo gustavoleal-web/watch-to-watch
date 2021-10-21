b584a51dd20dafbcf82ec621943bd2a9277f0381
gustavoleal-web <lealgustavo.web@gmail.com>
2021-10-21 14:37:01 -0500

    changed inline style to class

--- src/Components/Movies/Recommendations.js
+++ src/Components/Movies/Recommendations.js
@@ -1,18 +1,19 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom'
+import styles from './css/recommend.module.css';
 
 const CarouselComp = ( { recommendations } ) => {
 
     return <Fragment>
-        <h5 style={ { color: 'rgb(180, 186, 210)' } }>Recommendations</h5>
-        <div style={ { backgroundColor: 'rgb(180, 186, 210)' } }>
+        <h4>Recommendations</h4>
+        <div className={ styles.mainContainer }>
             {
                 recommendations.map( movie =>
-                    <div style={ { border: '5px solid white', margin: '5px 10px' } } key={ movie.id }>
+                    <div className={ styles.posterContainer } key={ movie.id }>
                         <Link to={ `/movies/trending/${ movie.id }` }>
                             <img
                                 src={ `https://image.tmdb.org/t/p/w300/${ movie.poster_path }` }
-                                alt="First slide"
+                                alt='movie poster'
                                 style={ { width: '200px' } }
                             />
                         </Link>
