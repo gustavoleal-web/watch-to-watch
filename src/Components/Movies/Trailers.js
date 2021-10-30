6623df0cdedf1fa5ccf38018e787a5602c4a27c2
gustavoleal-web <lealgustavo.web@gmail.com>
2021-10-30 18:49:32 -0500

    updated width and height

--- src/Components/Movies/Trailers.js
+++ src/Components/Movies/Trailers.js
@@ -1,23 +1,34 @@
 import React, { Fragment } from 'react';
 import ReactPlayer from 'react-player/youtube';
+import styles from './css/trailers.module.css'
 
 const Trailers = ( { videos } ) => {
-    return <div>
+    return <Fragment>
         <h4>Trailers</h4>
-        {
-            videos.map( video => <Fragment key={ video.id } >
-                <ReactPlayer
-                    url={ `https://www.youtube.com/watch?v=${ video.key }` }
-                    controls={ true }
-                    light={ true }
-                    height='200px'
-                    width='300px'
-                />
-                <p style={ { color: 'white' } }>{ video.name }</p>
-            </Fragment>
-            )
-        }
-    </div >
+
+        <div className={ styles.playerMainContainer }>
+            {
+                videos.map( video => {
+                    if ( video.site === 'YouTube' ) {
+                        return <div key={ video.id } >
+                            <ReactPlayer
+                                url={ `https://www.youtube.com/watch?v=${ video.key }` }
+                                controls={ true }
+                                light={ true }
+                                width='17rem'
+                                height='10rem'
+                                className={ styles.reactPlayer }
+                            />
+                            <p style={ { color: 'white' } }>{ video.name }</p>
+                        </div>
+
+                    }
+                    return true;
+                }
+                )
+            }
+        </div>
+    </Fragment >
 }
 
 export default Trailers;
