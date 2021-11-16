53e4c10ef1b6e6de25970cc6e04510f3cb7402f0
gustavoleal-web <lealgustavo.web@gmail.com>
2021-11-16 13:41:03 -0600

    import styles from movies comp

--- src/Components/TV/About.js
+++ src/Components/TV/About.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card'
-import styles from './css/about.module.css'
-import OffCanvasExample from './OfCanvas';
+import stylesTwo from './css/about.module.css'
+
+import MenuOfCanvas from '../Header/menuOfCanvas';
+import styles from '../Movies/css/about.module.css';
+import Sypnosis from '../Movies/Accordion';
 
 const About = () => {
     const [ state, setState ] = useState( {} )
     const params = useParams();
-    
+
     useEffect( () => {
         const fetchShows = async () => {
 
@@ -43,6 +46,7 @@ const About = () => {
 
     return (
         <Fragment>
+            <MenuOfCanvas type='shows' />
             <div className={ styles.mainContainer }>
                 {
                     Object.keys( state ).length === 0 ? null
@@ -55,48 +59,40 @@ const About = () => {
                                 />
 
                                 <div style={ { margin: 'auto' } }>
-                                    <h1 style={ { textAlign: 'center' } }>{ state.original_name }</h1>
+                                    <h3 style={ { textAlign: 'center' } }>{ state.name }</h3>
+                                    <h6 style={ { textAlign: 'center' } }>{ state.original_name }</h6>
 
                                     <p>Air Date: { state.first_air_date.replaceAll( '-', '/' ) }</p>
                                     { nextEpisodeDate }
                                 </div>
 
+                                <div className={ styles.generalInfo }>
+                                    <p>Type</p>
+                                    <p>Lang</p>
+                                    <p>Episodes</p>
 
+                                    <h6 className={ styles.title }>TV</h6>
+                                    <h6 className={ styles.title }>{ state.original_language.toUpperCase() }</h6>
+                                    <h6 className={ styles.title }>{ state.number_of_episodes }</h6>
+                                </div>
                             </div>
                             <p style={ { fontSize: '11px', textAlign: 'end', fontWeight: '600', color: 'lightGray' } }>YYYY/DD/MM</p>
 
-
-                            <Card bsPrefix={ styles.card }>
-                                <p>Type</p>
-                                <p>Lang</p>
-                                <p>Episodes</p>
-                                <p>Run time</p>
-
-
-                                <h6>TV</h6>
-                                <h6>{ state.original_language.toUpperCase() }</h6>
-                                <h6>{ state.number_of_episodes }</h6>
-                                <h6>{ state.episode_run_time[ 0 ] } min</h6>
-
-                            </Card>
-
-                            <Card bsPrefix={ styles.cardOverview }>
-                                <Card.Body>
-                                    { state.overview }
-                                </Card.Body>
-                            </Card>
+                            <Sypnosis overview={ state.overview } />
 
 
                             <Card bsPrefix={ styles.statsGenres }>
-                                <Card.Title className={ styles.statsGenresMargin }>Status</Card.Title>
-                                <Card.Title>Genres</Card.Title>
+                                <Card.Title className={ styles.statusTitle }>Status</Card.Title>
+                                <p className={ styles.movieStatus } >{ state.status }</p>
 
-                                <p>{ state.status }</p>
-                                <ul className={ styles.statsGenresUl }>
+                                <Card.Title className={ styles.genresTitle }>Genres</Card.Title>
+                                <ul className={ styles.genres }>
+
 
                                     {
                                         state.genres.map( genre =>
-                                            <li key={ genre.id }>
+                                            <li key={ genre.id }
+                                                className={ styles.genre }>
                                                 { genre.name }.
                                             </li>
                                         )
@@ -105,17 +101,17 @@ const About = () => {
                             </Card>
 
 
-                            <Card>
+                            <div className={ stylesTwo.seasons }>
                                 { state.seasons.map( season =>
-                                    <Fragment key={ season.season_number }>
+                                    <div key={ season.season_number }>
                                         <img src={ `https://image.tmdb.org/t/p/w300${ season.poster_path }` } alt='poster' style={ { height: '200px', width: '135px' } } />
                                         <p>{ season.name }</p>
-                                        <p>{ season.episode_count }</p>
-                                        <OffCanvasExample overview={ season.overview } />
-                                    </Fragment>
+                                        <p>Episodes { season.episode_count }</p>
+
+                                    </div>
                                 ) }
 
-                            </Card>
+                            </div>
 
                             <Card>
                                 <img src={ `https://image.tmdb.org/t/p/w300/${ state.backdrop_path }` } alt='poster' />
