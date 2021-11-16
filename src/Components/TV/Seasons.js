658b85e540bfbadfd84999d112a54b55625961dc
gustavoleal-web <lealgustavo.web@gmail.com>
2021-11-16 14:47:13 -0600

    incled episode count

--- src/Components/TV/Seasons.js
+++ src/Components/TV/Seasons.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import CollectionPoster from '../Movies/CollectioPoster';
 import styles from '../Movies/css/collection.module.css';
@@ -15,7 +15,7 @@ const Collection = ( { seasons, name } ) => {
         <>
             <span onClick={ handleShow } className={ styles.collectionName }>
                 <p>
-                   Seasons
+                    Seasons
                 </p>
                 <img src={ infoIcon } alt="" />
             </span>
@@ -23,17 +23,24 @@ const Collection = ( { seasons, name } ) => {
 
             <Offcanvas show={ show } onHide={ handleClose } placement="end">
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>{name}</Offcanvas.Title>
+                    <Offcanvas.Title>{ name }</Offcanvas.Title>
                 </Offcanvas.Header>
 
                 <Offcanvas.Body bsPrefix={ styles.canvasBody }>
                     {
-                        seasons.map( ( s ) => <CollectionPoster
-                            posterPath={ s.poster_path }
-                            title={ s.name }
-                            key={ s.id } /> )
+                        seasons.map( ( s ) =>
+                            <div key={ s.id}>
+                                <CollectionPoster
+                                    posterPath={ s.poster_path }
+                                    title={ s.name }
+                                    key={ s.id }
+                                />
+                                <h6 style={ { padding: '5px 10px' } }>Episodes { s.episode_count }</h6>
+                            </div>
+                        )
 
                     }
+
                 </Offcanvas.Body>
             </Offcanvas>
         </>
