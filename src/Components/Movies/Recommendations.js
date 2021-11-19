df3f2142c5cc4633e0ca0ea44624280444bfcd81
gustavoleal-web <lealgustavo.web@gmail.com>
2021-11-18 20:39:25 -0600

    update comp to work with tv and movies

--- src/Components/Movies/Recommendations.js
+++ src/Components/Movies/Recommendations.js
@@ -2,20 +2,20 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom'
 import styles from './css/recommend.module.css';
 
-const CarouselComp = ( { recommendations } ) => {
+const CarouselComp = ( { recommendations, mediaType } ) => {
     return <Fragment>
         <h4>Recommendations</h4>
         <div className={ styles.mainContainer }>
             {
-                recommendations.map( movie => {
-                    let imgSource = `https://image.tmdb.org/t/p/w300${ movie.poster_path }`;
+                recommendations.map( r => {
+                    let imgSource = `https://image.tmdb.org/t/p/w300${ r.poster_path }`;
                     return (
-                        <div className={ styles.posterContainer } key={ movie.id }>
+                        <div className={ styles.posterContainer } key={ r.id }>
 
-                            <Link to={ `/movies/trending/${ movie.id }` }>
+                            <Link to={ `/${ mediaType }/trending/${ r.id }` }>
                                 <img
                                     src={ imgSource }
-                                    alt='movie poster'
+                                    alt='poster'
                                     className={ styles.poster }
                                 />
                             </Link>
