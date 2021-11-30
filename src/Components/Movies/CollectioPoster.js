50c6d79815eafbf33934e767fdb703c4621541bb
gustavoleal-web <lealgustavo.web@gmail.com>
2021-11-30 12:01:32 -0600

    made movie poster into Link and prevent rerender of same movie

--- src/Components/Movies/CollectioPoster.js
+++ src/Components/Movies/CollectioPoster.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import noImage from '../../Images/No-Image-Placeholder.png';
 import styles from './css/collection.module.css'
 
-const CollectionPoster = ( { posterPath, title } ) => {
-
+const CollectionPoster = ( { posterPath, title, partId, movieId, handleClose } ) => {
     let posterSrc;
+    let intMovieId = parseInt( movieId );
 
     if ( posterPath === null ) {
         posterSrc = noImage;
@@ -14,12 +15,22 @@ const CollectionPoster = ( { posterPath, title } ) => {
                 <img src={ posterSrc } alt={ title } className={ styles.poster } />
                 <p style={ { fontSize: '14px' } }>{ title }</p>
             </span>
+
+
         )
+    }
+    //prevents the unnecessary rerender of the same movie
+    else if ( partId === intMovieId && !isNaN( movieId )) {
+        return <span>
+            <img src={ `https://image.tmdb.org/t/p/w300${ posterPath }` } alt={ title } className={ styles.poster } />
+        </span>
     }
 
     else {
         return (
-            <img src={ `https://image.tmdb.org/t/p/w300${ posterPath }` } alt={ title } className={ styles.poster } />
+            <Link to={ `/movies/trending/${ partId }` } onClick={ handleClose }>
+                <img src={ `https://image.tmdb.org/t/p/w300${ posterPath }` } alt={ title } className={ styles.poster } />
+            </Link>
         )
     }
 
