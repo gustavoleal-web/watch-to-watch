aefbfd3c356e982cf69542cf39e2228ef22bd660
gustavoleal-web <lealgustavo.web@gmail.com>
2021-12-11 19:17:21 -0600

    renders the first episode.

--- src/Components/TV/SeasonDetails.js
+++ src/Components/TV/SeasonDetails.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const SeasonDetails = () => {
-    return <div>
-        more season details
+const SeasonDetails = ( { season } ) => {
+    console.log( season );
+    return <div style={ { display: 'grid', gridTemplateColumns: '1fr 2fr', width: '100%', border: '1px solid black' } }>
+        <img style={{height: '100%', width:'100px'}} src={ `https://image.tmdb.org/t/p/w300/${ season.episodes[ 0 ].still_path  }` } alt="" />
+        <div>
+            <h6>{ season.episodes[ 0 ].name }</h6>
+            <p>{ season.episodes[ 0 ].overview }</p>
+        </div>
     </div>
 }
 
