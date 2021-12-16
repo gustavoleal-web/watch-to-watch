9409b5aa69ec682eda243b004cebe536390ea895
gustavoleal-web <lealgustavo.web@gmail.com>
2021-12-16 13:38:28 -0600

    added inline styles

--- src/Components/TV/SeasonDetails.js
+++ src/Components/TV/SeasonDetails.js
@@ -1,21 +1,21 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
+import MenuOfCanvas from '../Header/menuOfCanvas';
+// import Seasons from './Seasons';
 import Card from 'react-bootstrap/Card'
 import CardGroup from 'react-bootstrap/CardGroup';
+import noImage from '../../Images/No-Image-Placeholder.png'
 
 const SeasonDetails = () => {
     const [ seasonDetails, setSeasonDetails ] = useState( {} )
     const params = useParams();
-
-
 
     useEffect( () => {
         const fetchSeasonDetails = async () => {
             try {
                 let response = await axios.get( `/show/season/?showId=${ params.showId }&seasonNum=${ params.seasonNumber }` );
                 let results = response.data.results;
-                console.log( results )
                 setSeasonDetails( results );
                 // setSeasonNumber( results.season_number );
             }
@@ -33,26 +33,48 @@ const SeasonDetails = () => {
     }
 
     return <Fragment>
+        <MenuOfCanvas type='shows' />
         {
-            seasonDetails.episodes.map( ( e ) => <CardGroup key={ e.id }
-                style={ {
-                    width: '85%',
-                    margin: 'auto',
-                    paddingTop: '20px',
-                    marginBottom: '20px'
-                } }>
-                <Card>
-                    <Card.Img variant="top" src={ `https://image.tmdb.org/t/p/w500/${ e.still_path }` } style={ { height: '170px' } } />
-                    <Card.Body>
-                        <Card.Title>{ e.name }</Card.Title>
-                        <p style={ { fontSize: '14px' } }>{ e.overview }</p>
-                    </Card.Body>
-                    <Card.Footer style={ { display: 'flex', justifyContent: 'space-between' } }>
-                        <small className="text-muted">Release date: { e.air_date }</small>
-                        <small className="text-muted">{ e.episode_number }</small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
+            seasonDetails.episodes.map( ( e ) => {
+                let imgSrc = null;
+                if ( e.still_path === null ) {
+                    imgSrc = <Card.Img
+                        variant="top"
+                        src={ noImage }
+                        style={ { height: '170px', width: '200px', margin: 'auto', marginTop: '20px' } }
+                    />
+                }
+                else {
+                    imgSrc = <Card.Img
+                        variant="top"
+                        src={ `https://image.tmdb.org/t/p/w500/${ e.still_path }` }
+                        style={ { height: '170px' } }
+                    />
+                }
+
+                return ( <CardGroup key={ e.id }
+                    style={ {
+                        width: '85%',
+                        margin: 'auto',
+                        paddingTop: '20px',
+                        marginBottom: '20px'
+                    } }>
+                    <Card>
+                        { imgSrc }
+                        <Card.Body>
+                            <Card.Title>{ e.name }</Card.Title>
+                            <p style={ { fontSize: '14px' } }>{ e.overview }</p>
+                        </Card.Body>
+                        <Card.Footer style={ { display: 'flex', justifyContent: 'space-between' } }>
+                            <small className="text-muted">Release date: { e.air_date }</small>
+                            <small className="text-muted">{ e.episode_number }</small>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup> )
+
+            }
+
+
             )
         }
 
