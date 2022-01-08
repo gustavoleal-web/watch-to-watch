a2ec11fdae7681a63ffb5793b650bcdb62b17065
gustavoleal-web <lealgustavo.web@gmail.com>
2022-01-08 13:12:16 -0600

    added hook to fetch selected genre

--- src/Components/Movies/SelectedGenre.js
+++ src/Components/Movies/SelectedGenre.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const SelectedGenre = () => {
     const params = useParams();
-    console.log(params)
-    return <div>Genre</div>
+    const [ mediaByGenre, setMediaByGenre ] = useState( [] );
+
+    useEffect( () => {
+        const fetchMediaByGenres = async () => {
+
+            try {
+                let response = await axios.get( `/${ params.type }/byGenre/?genreId=${ params.genreId }` );
+
+                console.log( response.data.results.results );
+                //setGenres( results );
+            }
+            catch ( e ) {
+                console.log( e )
+            }
+        }
+        fetchMediaByGenres();
+    }, [ params.type, params.genreId ] );
+
+
+
+    console.log( params )
+    return <div>{ params.genreOption }</div>
 }
 
 export default SelectedGenre;
