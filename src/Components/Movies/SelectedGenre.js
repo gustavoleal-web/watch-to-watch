3f430f541cae6bb97d5ddc4d20c9ecec853055fd
gustavoleal-web <lealgustavo.web@gmail.com>
2022-01-11 18:49:07 -0600

    imported styles from movies css

--- src/Components/Movies/SelectedGenre.js
+++ src/Components/Movies/SelectedGenre.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import Media from '../Movies/Media';
 import MenuOfCanvas from '../Header/menuOfCanvas';
+import styles from '../Movies/css/movies.module.css';
 import axios from 'axios';
+
 
 const SelectedGenre = () => {
     const params = useParams();
@@ -16,24 +18,24 @@ const SelectedGenre = () => {
 
                 let responseCopy = [ ...response.data.results.results ];
 
-                    //adding the same key found in movies so it matches with shows 
-                    //prevents the props from being undefined in the key name is different
-                    if ( responseCopy.length > 0 ) {
-                        for ( let i = 0; i < responseCopy.length; i++ ) {
-                            if ( responseCopy[ i ].name ) {
-                                responseCopy[ i ].title = responseCopy[ i ].name;
-                                responseCopy[ i ].original_title = responseCopy[ i ].original_name;
-                            }
-                            if ( responseCopy[ i ].first_air_date ) {
-                                responseCopy[ i ].release_date = responseCopy[ i ].first_air_date;
-                            }
-
+                //adding the same key found in movies so it matches with shows 
+                //prevents the props from being undefined in the key name is different
+                if ( responseCopy.length > 0 ) {
+                    for ( let i = 0; i < responseCopy.length; i++ ) {
+                        if ( responseCopy[ i ].name ) {
+                            responseCopy[ i ].title = responseCopy[ i ].name;
+                            responseCopy[ i ].original_title = responseCopy[ i ].original_name;
                         }
+                        if ( responseCopy[ i ].first_air_date ) {
+                            responseCopy[ i ].release_date = responseCopy[ i ].first_air_date;
+                        }
+
                     }
+                }
 
 
-                setMediaByGenre(responseCopy );
-               
+                setMediaByGenre( responseCopy );
+
             }
             catch ( e ) {
                 console.log( e )
@@ -45,23 +47,28 @@ const SelectedGenre = () => {
     if ( mediaByGenre.length > 0 ) {
         return <Fragment>
             <MenuOfCanvas type={ params.type } />
-            
-            <h1>{ params.genreOption }</h1>
 
-            {
-                mediaByGenre.map( ( g ) =>
-                    <Media
-                        key={ g.id }
-                        id={ g.id }
-                        title={ g.title }
-                        releaseDate={ g.release_date }
-                        posterPath={ g.poster_path }
-                        rating={ g.vote_average }
-                        option='trending'
-                        type={ params.type } />
-                )
-            }
+            <div className={ styles.mainContainer }>
 
+                <h2 className={ styles.title }> { params.genreOption }</h2>
+
+                <div className={ styles.moviesContainer }>
+
+                    {
+                        mediaByGenre.map( ( g ) =>
+                            <Media
+                                key={ g.id }
+                                id={ g.id }
+                                title={ g.title }
+                                releaseDate={ g.release_date }
+                                posterPath={ g.poster_path }
+                                rating={ g.vote_average }
+                                option='trending'
+                                type={ params.type } />
+                        )
+                    }
+                </div>
+            </div>
         </Fragment >
 
 
