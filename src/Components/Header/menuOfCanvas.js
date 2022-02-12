84b1263479c937bcb0547b93c9423c1c0aac434c
gustavoleal-web <lealgustavo.web@gmail.com>
2022-02-12 11:32:51 -0600

    moved form options to new comp

--- src/Components/Header/menuOfCanvas.js
+++ src/Components/Header/menuOfCanvas.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import NavOptions from './NavOptions';
+import CustomSearch from './CustomSearch';
 import Dropdown from 'react-bootstrap/Dropdown';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+//import Form from 'react-bootstrap/Form';
+//import Row from 'react-bootstrap/Row';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
-import Accordion from 'react-bootstrap/Accordion';
+//import Accordion from 'react-bootstrap/Accordion';
 import { NavLink } from 'react-router-dom';
 import styles from './css/menu.module.css'
 import menu from '../../Images/menu.png';
@@ -131,88 +132,7 @@ const MenuOfCanvas = ( { type } ) => {
 
                         <Dropdown.Divider />
 
-                        {/* these will have to be looped */ }
-                        <Accordion>
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Year</Accordion.Header>
-                                <Accordion.Body>
-                                    <Form>
-                                        <Form.Group controlId="formGridCity">
-                                            <Form.Select size='sm'>
-                                                <option>Open this select menu</option>
-                                                <option value="2020">2020</option>
-                                                <option value="2021">2021</option>
-                                                <option value="2022">2022</option>
-                                            </Form.Select>
-                                        </Form.Group>
-                                    </Form>
-
-                                </Accordion.Body>
-                            </Accordion.Item>
-
-                            <Accordion.Item eventKey="1">
-                                <Accordion.Header>Date Range</Accordion.Header>
-                                <Accordion.Body>
-                                    <div>
-                                        <Form.Group controlId="formGridState">
-                                            <Form.Label>From</Form.Label>
-                                            <input type='date' />
-                                        </Form.Group>
-
-                                        <Form.Group controlId="formGridZip">
-                                            <Form.Label>To</Form.Label>
-                                            <input type='date' />
-                                        </Form.Group>
-                                    </div>
-                                </Accordion.Body>
-                            </Accordion.Item>
-
-                            <Accordion.Item eventKey="2">
-                                <Accordion.Header>Lang and Rating</Accordion.Header>
-                                <Accordion.Body>
-
-                                    <Form.Group controlId="formGridState">
-                                        <Form.Select size='sm'>
-                                            <option value="ENG" default>ENG</option>
-                                            <option value="SPA">SPA</option>
-                                            <option value="JAP">JAP</option>
-                                        </Form.Select>
-                                    </Form.Group>
-
-                                    <Form.Group controlId="formGridZip">
-                                        <Form.Select size='sm'>
-                                            <option>Rating</option>
-                                            <option value="10">10</option>
-                                            <option value="9">9</option>
-                                            <option value="8">8</option>
-                                        </Form.Select>
-                                    </Form.Group>
-
-                                </Accordion.Body>
-                            </Accordion.Item>
-
-                            <Accordion.Item eventKey="3">
-                                <Accordion.Header>Genre</Accordion.Header>
-
-                                <Accordion.Body>
-                                    <Form.Group controlId="formGridZip">
-                                        <Form.Select size='sm'>
-                                            <option value="Action">Action</option>
-                                            <option value="Advanture">Advanture</option>
-                                            <option value="Horror">Horror</option>
-                                        </Form.Select>
-                                    </Form.Group>
-                                </Accordion.Body>
-
-                            </Accordion.Item>
-
-
-                        </Accordion>
-
-
-
-
-
+                        <CustomSearch/>
                     </Dropdown.Menu>
 
                 </Offcanvas.Body>
