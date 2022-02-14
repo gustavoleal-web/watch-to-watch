38837ecb51d9e11883a363494d6a4fdc43fcd049
gustavoleal-web <lealgustavo.web@gmail.com>
2022-02-14 09:12:59 -0600

    added stated and rendered media

--- src/Components/Movies/CutomSearchResults.js
+++ src/Components/Movies/CutomSearchResults.js
@@ -1,17 +1,33 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import MenuOfCanvas from '../Header/menuOfCanvas';
+import Media from '../Movies/Media';
+import styles from './css/movies.module.css';
 import { useParams } from 'react-router';
 import axios from 'axios';
 
 const CustomSearchResults = () => {
     const params = useParams();
-    console.log( params );
+
+    const [ state, setState ] = useState( {
+        title: '',
+        list: [],
+        dates: null
+    } );
 
     useEffect( () => {
         const fetchSearchOptions = async () => {
             try {
-                const response = await axios.get( `/${ params.type }/releaseYear/language/?year=${ params.releaseYear }&originalLang=${ params.language }` );
+                const response = await axios.get(
+                    `/${ params.type }/releaseYear/genre/language/rating/?year=${ params.releaseYear }&originalLang=${ params.language }&rating=${ params.rating }&genre=${ params.genre }` );
+
                 let results = response.data.results;
-                console.log( results );
+                console.log( results.results );
+
+                setState( {
+                    title: params.type,
+                    list: results.results,
+                    dates: null
+                } )
             }
 
             catch ( error ) {
@@ -19,11 +35,37 @@ const CustomSearchResults = () => {
             }
         }
         fetchSearchOptions();
-    }, [ params.type, params.releaseYear, params.language ] )
+    }, [ params ] )
 
-    return <Fragment>
-    <h5>Custom Search Results</h5>
-    </Fragment>
+    if ( state.list.length === 0 ) {
+        return <h1>No results found</h1>
+    }
+
+    else {
+        return <Fragment>
+            <MenuOfCanvas type={ params.type } />
+
+            <h2 className={ styles.title }>{ params.type } results</h2>
+
+            { state.list.map( item =>
+                <Media
+                    key={ item.id }
+                    id={ item.id }
+                    title={ item.title }
+                    releaseDate={ item.release_date }
+                    posterPath={ item.poster_path }
+                    rating={ item.vote_average }
+                    option='customResults'
+                    type={ params.type }
+                />
+            ) }
+        </Fragment>
+
+
+
+    }
+
+
 }
 
 export default CustomSearchResults;
