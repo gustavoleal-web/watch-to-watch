77858981d9758b3ba4e91850ce0fac47dfeaafc1
gustavoleal-web <lealgustavo.web@gmail.com>
2022-03-31 11:24:19 -0500

    added infinite scroll and func to fetch more data

--- src/Components/Movies/SelectedGenre.js
+++ src/Components/Movies/SelectedGenre.js
@@ -4,19 +4,26 @@ import Media from '../Movies/Media';
 import MenuOfCanvas from '../Header/menuOfCanvas';
 import styles from '../Movies/css/movies.module.css';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 
 const SelectedGenre = () => {
     const params = useParams();
     const [ mediaByGenre, setMediaByGenre ] = useState( [] );
 
+    const [ currentPage, setCurrentPage ] = useState( 1 );
+    const [ maxPages, setMaxPages ] = useState( 0 );
+    const [ hasMore, setHasMore ] = useState( true );
+
     useEffect( () => {
         const fetchMediaByGenres = async () => {
 
             try {
-                let response = await axios.get( `/${ params.type }/byGenre/?genreId=${ params.genreId }` );
+                let response = await axios.get( `/${ params.type }/byGenre/?genreId=${ params.genreId }&page=${ 1 }` );
 
                 let responseCopy = [ ...response.data.results.results ];
+                console.log( response.data.results.results )
 
                 //adding the same key found in movies so it matches with shows 
                 //prevents the props from being undefined in the key name is different
@@ -33,8 +40,8 @@ const SelectedGenre = () => {
                     }
                 }
 
-
                 setMediaByGenre( responseCopy );
+                setMaxPages( response.data.results.total_pages )
 
             }
             catch ( e ) {
@@ -43,6 +50,23 @@ const SelectedGenre = () => {
         }
         fetchMediaByGenres();
     }, [ params.type, params.genreId ] );
+
+
+    const fetchMoreData = async () => {
+        if ( currentPage > ( maxPages / 4 ) ) {
+            setHasMore( false );
+            return;
+        }
+
+        let nextPage = currentPage + 1;
+        let response = await axios.get( `/${ params.type }/byGenre/?genreId=${ params.genreId }&page=${ nextPage }`);
+
+        const updatedGenreList = [ ...mediaByGenre, ...response.data.results.results ];
+
+        setMediaByGenre( updatedGenreList )
+        setCurrentPage( nextPage );
+    }
+
 
     if ( mediaByGenre.length > 0 ) {
         return <Fragment>
@@ -54,20 +78,34 @@ const SelectedGenre = () => {
 
                 <div className={ styles.moviesContainer }>
 
-                    {
-                        mediaByGenre.map( ( g ) =>
-                            <Media
-                                key={ g.id }
-                                id={ g.id }
-                                title={ g.title }
-                                releaseDate={ g.release_date }
-                                posterPath={ g.poster_path }
-                                rating={ g.vote_average }
-                                option='trending'
-                                type={ params.type } 
-                                overview={ g.overview }/>
-                        )
-                    }
+                    <InfiniteScroll
+                        dataLength={ mediaByGenre.length }
+                        next={ fetchMoreData }
+                        hasMore={ hasMore }
+                        loader={ <h4>Loading...</h4> }
+                        endMessage={
+                            <p style={ { textAlign: 'center' } }>
+                                <b>End of the line.</b>
+                            </p> }>
+
+                        <div>
+                            {
+                                mediaByGenre.map( ( g ) =>
+                                    <Media
+                                        key={ g.id }
+                                        id={ g.id }
+                                        title={ g.title }
+                                        releaseDate={ g.release_date }
+                                        posterPath={ g.poster_path }
+                                        rating={ g.vote_average }
+                                        option='trending'
+                                        type={ params.type }
+                                        overview={ g.overview } />
+                                )
+                            }
+                        </div>
+                    </InfiniteScroll>
+
                 </div>
             </div>
         </Fragment >
